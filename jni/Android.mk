f5fdfdb38091c8b61e39d95c9752c2f27cb2e2b4
mutz555 <mrbosscoc22@gmail.com>
2025-05-02 14:20:12 +0700

    Add dobby.a and update Android.mk for build

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,9 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
+# Modul utama
 include $(CLEAR_VARS)
 LOCAL_MODULE := FingerprintBypassModule
 LOCAL_SRC_FILES := main.cpp hook.cpp config.cpp module.cpp
-LOCAL_STATIC_LIBRARIES := libdobby libcxx_static 
+LOCAL_STATIC_LIBRARIES := libdobby liblsplant libcxx_static
 LOCAL_LDLIBS := -llog
 LOCAL_CFLAGS := -std=c++20 -Wall -Wextra -fno-rtti -fvisibility=hidden
 LOCAL_CPPFLAGS := -std=c++20
@@ -11,12 +12,6 @@ include $(BUILD_SHARED_LIBRARY)
 
 # Dobby library
 include $(CLEAR_VARS)
-LOCAL_MODULE := dobby
-LOCAL_SRC_FILES := $(LOCAL_PATH)/libs/$(TARGET_ARCH_ABI)/libdobby.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# LSPlant library
-include $(CLEAR_VARS)
-LOCAL_MODULE := lsplant
-LOCAL_SRC_FILES := $(LOCAL_PATH)/libs/$(TARGET_ARCH_ABI)/liblsplant.a
+LOCAL_MODULE := libdobby
+LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libdobby.a
 include $(PREBUILT_STATIC_LIBRARY)
