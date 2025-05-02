33f29d91d3ad039b3c071cbb4c83584e2148e2c5
mutz555 <mrbosscoc22@gmail.com>
2025-05-02 16:37:49 +0700

    Update Logger.hpp

--- jni/include/Logger.hpp
+++ jni/include/Logger.hpp
@@ -1,24 +1,54 @@
 #pragma once
-#include <android/log.h>
 
-#define TAG "FPBypass"
+#include <jni.h>
+#include <android/log.h>
+#include <string>
+#include <filesystem>
+#include <cstdio>
+#include <ctime>
+#include <error.h>
+
+#define LOG_TAG "FpBypass"
+#define LOGI(...) if (Logger::isDebugEnabled()) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) if (Logger::isDebugEnabled()) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 class Logger {
 public:
-    static void debug(const char* msg) {
-        __android_log_print(ANDROID_LOG_DEBUG, TAG, "%s", msg);
+    static void setLogFile(const std::string& path) {
+        logFile = path;
     }
 
-    static void warn(const char* msg) {
-        __android_log_print(ANDROID_LOG_WARN, TAG, "%s", msg);
+    static void setDebugEnabled(bool enabled) {
+        debugEnabled = enabled;
     }
 
-    // Versi overload untuk string C++
-    static void debug(const std::string& msg) {
-        debug(msg.c_str());
+    static bool isDebugEnabled() {
+        return debugEnabled;
     }
 
-    static void warn(const std::string& msg) {
-        warn(msg.c_str());
+    static void info(const std::string& message) {
+        LOGI("%s", message.c_str());
+        writeToFile("INFO", message);
     }
-};
+
+    static void error(const std::string& message) {
+        LOGE("%s", message.c_str());
+        writeToFile("ERROR", message);
+    }
+
+private:
+    static void writeToFile(const std::string& level, const std::string& message) {
+        if (!debugEnabled || logFile.empty()) return;
+
+        std::filesystem::path path(logFile);
+        if (!path.parent_path().empty()) {
+            std::error_code ec;
+            std::filesystem::create_directories(path.parent_path(), ec);
+        }
+
+        FILE* file = fopen(logFile.c_str(), "a");
+        if (file) {
+            fprintf(file, "[%s] [%s] %s\n",
+                    getCurrentTimeString().c_str(),
+                    level.c_str(),
+                    message
